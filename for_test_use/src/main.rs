d485c8bb310c9cbb5e8af458068fdf5b867ccafd
Direct-A <songyicheng0@gmail.com>
2022-01-04 11:56:29 +0800

    update: microsoft 练习二

---
+++ for_test_use/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // let goods = vec![0; 5];
+    let mut goods = Vec::new();
+    goods.push(5);
+    goods.push(6);
+    goods.push(7);
+    goods.push(8);
+
+    println!("a new vector {:?}", goods);
+    println!("the last member of vector {}", goods[goods.len()-1]);
+}
