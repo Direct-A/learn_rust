7db63ed5fab6bc398afbd6dba6dea4bbeba2ca8b
Direct-A <songyicheng0@gmail.com>
2022-01-11 13:55:18 +0800

    rearrange floder, by learning course

--- for_test_use/src/main.rs
+++ for_test_use/src/main.rs
@@ -1,13 +1,15 @@
 fn main() {
-    let mut v = vec![1,2,3,4,5];
-    let first = v[0];
-    let mut second = "xxx".to_string();
-    second.push_str("ooo");
-    let mut third = String::from("xxxxxxx");
-    third.push_str(&second);
-    v.push(6);
-    println!("first: {}", first);
-    println!("v[0]: {}", v[0]);
-    println!("second: {}", second);
-    println!("third: {}", third);
+    let s = "नमस्ते";
+
+    let mut j = 1;
+    for i in s.bytes() {
+        println!("{}:\t{}", j, i);
+        j += 1;
+    }
+
+    let mut j = 1;
+    for i in s.chars() {
+        println!("{}:\t{}", j, i);
+        j += 1;
+    }
 }
